c21b7d94acef0eddbb16061270cd478e7f53d689
hammadtech16 <hammadriaz170@gmail.com>
2025-09-16 23:20:54 -0700

    recent changes

---
+++ my-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <nav class='bg-sky-400 flex justify-between items-center m-2 p-3 rounded-2xl '>
+      <div><img src="/src/assets/react.svg" alt="" srcset="" /></div>
+      <ul class='flex gap-8 pf-9   '>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+      <div class='flex gap-4'>
+        <button class='mx-8 bg-gray-800 '>Shop</button>
+        <button>Sign Up</button>
+      </div>
+    </nav>
+
+    </>
+  )
+}
+
+export default App
